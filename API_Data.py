a5901fbac8606454a441f0214fb10288e5e37e29
ardourApeX <56365512+ardourApeX@users.noreply.github.com>
2020-10-03 13:08:38 +0530

    Add files via upload

---
+++ API_Data.py
@@ -0,0 +1,9 @@
+# https://apps.twitter.com/
+# Create new app and get the four strings
+
+def oauth():
+	return {"consumer_key": None,
+		"consumer_secret": None,
+		"token_key" : None,
+		"token_secret": None
+	}
